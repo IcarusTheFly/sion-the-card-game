54a88fa472a87c91833532df86193336bfbe959d
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2024-08-24 20:49:31 +1000

    Re-style New Deck button

--- app/decks/DecksListNewDeck.tsx
+++ app/decks/DecksListNewDeck.tsx
@@ -4,15 +4,16 @@ import AddIcon from "../icons/AddIcon";
 export default function DecksListNewDeck() {
   return (
     <Link
-      className="border-5 border-dashed border-blue-300 rounded-md bg-[#2d4d9e] bg-opacity-50 p-4 text-lg text-gray-200 flex flex-col items-center justify-center
-      aspect-square transition-all
-    hover:bg-[#3055b0] hover:bg-opacity-100 hover:text-xl"
+      className="border-2 border-dashed border-[#ffd700] rounded-md bg-blue-900 bg-opacity-50 text-xl text-gray-200
+      flex flex-col items-center justify-center
+      aspect-square transition-colors duration-200
+    hover:bg-blue-800 "
       href="/decks/new"
       prefetch={false}
     >
       <AddIcon />
       {/* Add shadow to the text */}
-      <h3 className="mb-2 font-bold">Nuevo mazo</h3>
+      <h3 className="font-semibold">Nuevo mazo</h3>
     </Link>
   );
 }
